3db66b27399462ef86241ffaeed7154a5c07479b
Lars Wrenger <wrenger@sra.uni-hannover.de>
2024-03-26 10:38:00 +0100

    :bug: Update local names

--- tests/local.c
+++ tests/local.c
@@ -15,20 +15,20 @@ declare_test(local_last_free_inc)
 	bool success = true;
 
 	local_t local;
-	local_init(&local);
+	ll_local_init(&local);
 
 	for (size_t i = 0; i < LAST_FREES; i++) {
-		check(!local_free_inc(&local, 0));
+		check(!ll_local_free_inc(&local, 0));
 	}
-	check(local_free_inc(&local, 0));
-	check(local_free_inc(&local, 0));
+	check(ll_local_free_inc(&local, 0));
+	check(ll_local_free_inc(&local, 0));
 
-	check(!local_free_inc(&local, 1));
-	check(!local_free_inc(&local, 0));
+	check(!ll_local_free_inc(&local, 1));
+	check(!ll_local_free_inc(&local, 0));
 
 	for (size_t i = 0; i < LAST_FREES; i++) {
-		check(!local_free_inc(&local, 1));
+		check(!ll_local_free_inc(&local, 1));
 	}
-	check(local_free_inc(&local, 1));
+	check(ll_local_free_inc(&local, 1));
 	return success;
 }
